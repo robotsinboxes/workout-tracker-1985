1ad9d6dbce60b292576c1f493278e7d4607844b2
Lisa Goodell <robotsinboxes@gmail.com>
2021-05-12 20:37:48 -0500

    added code to routes, need to change vars to match workout_tracker app

--- Develop/routes/api/index.js
+++ Develop/routes/api/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
-// const statsRoutes = require('./statsRoutes');
+const statsRoutes = require('./statsRoutes');
 const exerciseRoutes = require('./exerciseRoutes');
 
-// router.use('/stats', statsRoutes);
+router.use('/stats', statsRoutes);
 router.use('/exercise', exerciseRoutes);
 
 module.exports = router;
--- Develop/server.js
+++ Develop/server.js
@@ -3,9 +3,12 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const routes = require("./routes")
 
+// do I need to change the atlas cluster to Workouts?***************************
 let uri = 'mongodb+srv://lisagoodell-admin:VWb88ijDMdgA4j2z@cluster0.4ns5m.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 
 const PORT = process.env.PORT || 3001;
+
+const db = require('./models');
 
 const app = express();
 
@@ -16,13 +19,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+app.use(app.router);
+routes.initialize(app);
+
 mongoose.connect("mongodb://localhost/workouts", {           
     useNewUrlParser: true,
     useFindAndModify: false,
     useUnifiedTopology: true,
 });
-
-app.use(routes);
 
 db.Workouts.create({ name: "Workouts" })
   .then(dbWorkouts => {
@@ -32,28 +36,7 @@ db.Workouts.create({ name: "Workouts" })
     console.log(message);
   });
 
-app.post("/submit", ({body}, res) => {
-  db.exercises.create(body)
-    .then(({_id}) => db.Workouts.findOneAndUpdate({}, { $push: { exercise: _id } }, { new: true }))
-    .then(dbWorkouts => {
-      res.json(dbWorkouts);
-    })
-    .catch(err => {
-      res.json(err);
-    });
-});
-
 app.get("/exercises", (req, res) => {
-  db.exercises.find({})
-    .then(dbExercises => {
-      res.json(dbExercises);
-    })
-    .catch(err => {
-      res.json(err);
-    });
-});
-
-app.get("/workouts", (req, res) => {
   db.Workouts.find({})
     .then(dbWorkouts => {
       res.json(dbWorkouts);
@@ -62,10 +45,31 @@ app.get("/workouts", (req, res) => {
       res.json(err);
     });
 });
+
+// app.get("/workouts", (req, res) => {
+//   db.Workouts.find({})
+//     .then(dbWorkouts => {
+//       res.json(dbWorkouts);
+//     })
+//     .catch(err => {
+//       res.json(err);
+//     });
+// });
 
 app.get("/stats", (req, res) => {
   db.Workouts.find({})
     .populate("stats")
+    .then(dbWorkouts => {
+      res.json(dbWorkouts);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+app.post("/submit", ({body}, res) => {
+  db.Workouts.create(body)
+    .then(({_id}) => db.Workouts.findOneAndUpdate({}, { $push: { exercise: _id } }, { new: true }))
     .then(dbWorkouts => {
       res.json(dbWorkouts);
     })
